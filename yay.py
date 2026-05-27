53a044dccc353875927a423a7dd722e1f671e10b
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-05-28 03:48:04 +0530

    Update yay.py

--- yay.py
+++ yay.py
@@ -1,4 +1,8 @@
 import streamlit as st
+container = st.container()
+with container:
+  st.title('Container')
 st.title('GitHub')
 st.header('IS')
 st.write('Complicated')
+st.sidebar.title('SidebaR')
