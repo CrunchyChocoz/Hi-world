d217af6e6b3442451aa658952c1ea286688c7224
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-05-29 00:42:59 +0530

    Update yay.py

--- yay.py
+++ yay.py
@@ -1,8 +1,11 @@
 import streamlit as st
 st.title('TIC TAC TOE')
-c1,c2 = st.columns([1,2], vertical_alignment='top', border=True)
+c1,c2 = st.columns([1,3], vertical_alignment='top', border=True)
 with c2:
   st.header('-by CrunchyChocoz')
-st.form('Enter a Username: ') 
-form
+with st.form('alias_form', border=True):
+  username = st.text_input('Enter an Alias')
+  submit_alias = st.form_submit_button('SUBMIT')
+  if submit_alias:
+    st.write('!!!!')
 st.sidebar.title('SidebaR')
