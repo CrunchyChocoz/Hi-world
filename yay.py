07cfcec2f8d437ae60bdf1f78d3bef5265074bb3
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-05-29 01:00:46 +0530

    Update yay.py

--- yay.py
+++ yay.py
@@ -2,9 +2,9 @@ import streamlit as st
 st.title('TIC TAC TOE')
 c1,c2 = st.columns([1,3], vertical_alignment='top', border=True)
 with c2:
-  st.header('-by CrunchyChocoz')
+  st.markdown(###'-*by CrunchyChocoz*')
 with st.form('alias_form', border=True):
-  st.markdown('### Enter an :gray[ALIAS]:')
+  st.markdown('## Enter an :gray[ALIAS]')
   username = st.text_input(label='', label_visibility='collapsed')
   submit_alias = st.form_submit_button('SUBMIT')
   if submit_alias:
