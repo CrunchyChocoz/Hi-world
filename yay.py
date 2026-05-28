92d9cc3f6cf423bec70dfd7ccfd3f0fb6b50506a
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-05-28 15:50:08 +0530

    Update yay.py

--- yay.py
+++ yay.py
@@ -5,10 +5,5 @@ container = st.container()
 with container:
   st.title('Container')
 c1,c2,c3 = st.columns(3, border=True)
-with c1:
-  st.title('GitHub')
-with c2:
-  st.header('IS')
-with c3:
   st.write('Complicated')
 st.sidebar.title('SidebaR')
