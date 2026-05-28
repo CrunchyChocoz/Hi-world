a01957d740bc98b17532142cc3ebdc5bc05f2697
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-05-29 00:59:21 +0530

    Update yay.py

--- yay.py
+++ yay.py
@@ -4,7 +4,7 @@ c1,c2 = st.columns([1,3], vertical_alignment='top', border=True)
 with c2:
   st.header('-by CrunchyChocoz')
 with st.form('alias_form', border=True):
-  st.markdown('## ENTER AN :gray[ALIAS]:')
+  st.markdown('### Enter an :gray[ALIAS]:')
   username = st.text_input(label='', label_visibility='collapsed')
   submit_alias = st.form_submit_button('SUBMIT')
   if submit_alias:
