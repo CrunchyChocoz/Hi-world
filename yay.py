16eda206041474888a3790fd1dd5c0a6ae42d733
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-05-28 16:01:00 +0530

    Update yay.py

--- yay.py
+++ yay.py
@@ -1,5 +1,7 @@
 import streamlit as st
 st.title('TIC TAC TOE')
-st.header('       -by CrunchyChocoz')
+c1,c2 = st.columns(2)
+with c2:
+  st.header('-by CrunchyChocoz')
 c1,c2,c3 = st.columns(3, border=True)
 st.sidebar.title('SidebaR')
