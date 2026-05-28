fddf6ff5ac2921024165f5cecb7dbc4670f623bc
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-05-28 16:55:56 +0530

    Update yay.py

--- yay.py
+++ yay.py
@@ -1,7 +1,8 @@
 import streamlit as st
 st.title('TIC TAC TOE')
-c1,c2 = st.columns([1,2], vertical_alignment='top')
+c1,c2 = st.columns([1,2], vertical_alignment='top', border=True)
 with c2:
   st.header('-by CrunchyChocoz')
-c1,c2,c3 = st.columns(3, border=True)
+ form_name = st.form('Enter a Username: ') 
+c1,c2 = st.columns(2, border=True)
 st.sidebar.title('SidebaR')
