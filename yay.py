ddb066fefd02199552cfe00b85a05f88b844ca5a
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-05-28 16:08:19 +0530

    Update yay.py

--- yay.py
+++ yay.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title('TIC TAC TOE')
-c1,c2 = st.columns([1,2], vertical_allignment='top')
+c1,c2 = st.columns([1,2], vertical_alignment='top')
 with c2:
   st.header('-by CrunchyChocoz')
 c1,c2,c3 = st.columns(3, border=True)
