1b72d76acef83c9033a4ab6d5628b861b1429bdc
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-05-29 00:28:32 +0530

    Update yay.py

--- yay.py
+++ yay.py
@@ -3,6 +3,6 @@ st.title('TIC TAC TOE')
 c1,c2 = st.columns([1,2], vertical_alignment='top', border=True)
 with c2:
   st.header('-by CrunchyChocoz')
-form_name = st.form('Enter a Username: ') 
-c1,c2 = st.columns(2, border=True)
+st.form('Enter a Username: ') 
+form
 st.sidebar.title('SidebaR')
