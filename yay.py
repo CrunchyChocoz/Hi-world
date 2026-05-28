ec4b8b5202a5ee1451ce9f011c6beb78ae23d558
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-05-28 15:48:38 +0530

    Update yay.py

--- yay.py
+++ yay.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title('TIC TAC TOE')
-st.header('    -by CrunchyChocoz')
+st.header('       -by CrunchyChocoz')
 container = st.container()
 with container:
   st.title('Container')
