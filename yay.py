6c7d969bd7b04b551b40d11ed1a9b0fe7818d04c
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-06-01 19:37:54 +0530

    Update yay.py

--- yay.py
+++ yay.py
@@ -9,7 +9,7 @@ c1,c2,c3 = st.columns(3)
 with c2:
   st.title('TIC TAC TOE')
 
-c4,c5 = st.columns(2, vertical_alignment='top', border=True)
+c4,c5 = st.columns(2, vertical_alignment='top')
 with c5:
   st.markdown('-*by CrunchyChocoz*')
 
