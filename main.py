363ce64ce85a92dae28cbb44a144c0321a8cc211
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-06-08 02:28:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit
 
-st.session_state.login_attained = False if 'login_attained' not in st.session_state
+st.session_state.setdefault('login_attained',False)
 
 page_login = st.page('Pages/yay.py', title='USER AUTHENTICATION', default=True)
 page_game = st.page('Pages/Otictactoe.py', title='OTICTACTOE')
