87d15d832fdbcdffc90dd229c372fdf8a99e2cd6
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-06-08 02:31:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ st.session_state.setdefault('login_attained',False)
 page_login = st.Page('Pages/yay.py', title='USER AUTHENTICATION', default=True)
 page_game = st.Page('Pages/Otictactoe.py', title='OTICTACTOE')
 
-router_page = st.page_navigation([page_login]) if st.session_state == False else st.page_navigation([page_login,page_game])
+router_page = st.navigation([page_login]) if st.session_state == False else st.page_navigation([page_login,page_game])
 router_page.run()
