6e0a15ee91e41e1eef88061df65710a8f2a25d29
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-07-08 00:18:19 +0530

    Update defaultOTTT.py

--- Pages/defaultOTTT.py
+++ Pages/defaultOTTT.py
@@ -6,13 +6,14 @@ st.divider()
 
 table = st.container(border=True)
 with table:
-  st.html('<div class="classic-board"></div>')
   st.html('''<style> div[data-testid="stVerticalBlockBorderWrapper"]:has(.classic-board)
               {background-color: #0000ff !important;
                border: 20px solid #ffffff !important;
                border-radius: 100px;
                padding: 20px;}
              </style>''')
+  st.html('<div class="classic-board"></div>')
+  
   c4,c5,c6 = st.columns(3,border=True)
   c7,c8,c9 = st.columns(3,border=True)
   c10,c11,c12 = st.columns(3,border=True)
