46a982d9fde449061a76a789a8f48ec29da3b2ee
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-07-07 16:19:56 +0530

    Update defaultTTT.py

--- Pages/defaultTTT.py
+++ Pages/defaultTTT.py
@@ -1,3 +1,13 @@
 import streamlit as st
-st.header('DOOOOOOOOOOOOOOONE')
+st.set_page_config(layout='wide')
+
+c1,c2,c3 = st.columns(3)
+with c2:
+  st.title('TIC TAC TOE')
+
+c4,c5 = st.columns(2, vertical_alignment='top')
+with c5:
+  st.markdown('-*by CrunchyChocoz*')
+
+st.divider()
 
