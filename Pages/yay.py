10242ec381c6a30e7514c4dbaddb57e095d2dcf8
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-07-07 16:20:37 +0530

    Update yay.py

--- Pages/yay.py
+++ Pages/yay.py
@@ -6,7 +6,7 @@ st.set_page_config(layout='wide')
 
 c1,c2,c3 = st.columns(3)
 with c2:
-  st.title('TIC TAC TOE')
+  st.title('OTIC TAC TOE')
 
 c4,c5 = st.columns(2, vertical_alignment='top')
 with c5:
