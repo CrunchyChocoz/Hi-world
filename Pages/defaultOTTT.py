6a34066986e45a8d3933867526922cc18b390aef
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-07-08 00:45:33 +0530

    Update defaultOTTT.py

--- Pages/defaultOTTT.py
+++ Pages/defaultOTTT.py
@@ -9,7 +9,7 @@ table = st.container(border=True)
 with table:
   st.html('''<style> div[data-testid="stLayoutWrapper"]:has(.classic-board)
               {background-color: #ffffff !important;
-               border: 20px solid #aaaaaa !important;
+               border: 20px solid #dddddd !important;
                border-radius: 0px;
                padding: 20px;}
              </style>''')
