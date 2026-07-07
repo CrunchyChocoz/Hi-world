3f2ed164c31fb34516581b74e7f214f8ad32eeb8
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-07-07 23:54:03 +0530

    Update defaultOTTT.py

--- Pages/defaultOTTT.py
+++ Pages/defaultOTTT.py
@@ -6,9 +6,12 @@ st.divider()
 
 table = st.container(border=True)
 with table:
-  st.html('''<style> div[data-testid="stVerticalBlockBorderWrapper"]{
-              background-color: #3b3f4b
-              ''')
+  st.html('''<style> div[data-testid="stVerticalBlockBorderWrapper"]
+              {background-color: #3b3f4b !important;
+               border: 2px solid !important;
+               border-radius: 100px
+               padding: 20px
+             </style>''')
 #  c4,c5,c6 = st.columns(3,border=True)
 #  c7,c8,c9 = st.columns(3,border=True)
 #  c10,c11,c12 = st.columns(3,border=True)
