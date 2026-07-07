7d1d66f14bcefbff41a2a9aa88a595ebb4423a89
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-07-07 22:01:15 +0530

    Update defaultOTTT.py

--- Pages/defaultOTTT.py
+++ Pages/defaultOTTT.py
@@ -5,7 +5,8 @@ c1,c2,c3 = st.columns([1,3,1])
 with c2:
   st.title('CLASSIC TICTACTOE')
 st.divider()
-
-c4,c5,c6 = st.columns(3, border=True)
-c7,c8,c9 = st.columns(3, border=True)
-c10,c11,c12 = st.columns(3, border=True)
+table = st.container(border=True)
+with table:
+  c4,c5,c6 = st.columns(3,border=True)
+  c7,c8,c9 = st.columns(3,border=True)
+  c10,c11,c12 = st.columns(3,border=True)
