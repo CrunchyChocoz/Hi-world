214903bbecdc68ec946335b59d6f01619b3a90bd
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-07-08 02:11:13 +0530

    Update defaultOTTT.py

--- Pages/defaultOTTT.py
+++ Pages/defaultOTTT.py
@@ -4,19 +4,17 @@ import random
 st.markdown('<h1 style="text-align: center;">CLASSIC TICTACTOE</h1>', unsafe_allow_html=True)
 
 st.divider()
-c1,c2,c3 = st.columns(3)
-with c2:
-  table = st.container(border=True)
-  with table:
-    st.html('''<style> div[data-testid="stLayoutWrapper"]:has(.classic-board)
-                {background-color: #ffffff !important;
-                 border: 5px solid #2D2C35 !important;
-                 border-radius: 0px;
-                 padding: 20px;}
-               </style>''')
+
+table = st.container(border=True)
+with table:
+  st.html('''<style> div[data-testid="stLayoutWrapper"]:has(.classic-board)
+                border: 5px solid #2D2C35 !important;
+                border-radius: 0px;
+                padding: 20px;}
+              </style>''')
   
-    st.html('<div class="classic-board"></div>')
+  st.html('<div class="classic-board"></div>')
     
-    c4,c5,c6 = st.columns(3,border=True)
-    c7,c8,c9 = st.columns(3,border=True)
-    c10,c11,c12 = st.columns(3,border=True)
+  c4,c5,c6 = st.columns(3,border=True)
+  c7,c8,c9 = st.columns(3,border=True)
+  c10,c11,c12 = st.columns(3,border=True)
