14a64b60ad5d0851138a844cec0a1027d66be273
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-07-07 16:27:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ st.session_state.setdefault('login_attained',False)
 
 page_login = st.Page('Pages/yay.py', title='USER AUTHENTICATION', default=True)
 page_Home = st.Page('Pages/Home.py', title='OTIC TAC TOE')
+page_DOTTT = st.page('Pages/defaultOTTT.py', title='Default OTTT')
+page_SOTTT = st.page('Pages/superOTTT.py', title='Super OTTT')
 
 if st.session_state.login_attained == False:
   router_page = st.navigation([page_login])  
 else:
-  router_page = st.navigation([page_Home])
+  router_page = st.navigation([page_Home,page_DOTTT,page_SOTTT])
 router_page.run()
