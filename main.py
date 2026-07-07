f841275864c48e9d55969f84a3617cf48a47b036
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-07-07 16:22:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import streamlit as st
 st.session_state.setdefault('login_attained',False)
 
 page_login = st.Page('Pages/yay.py', title='USER AUTHENTICATION', default=True)
-page_game = st.Page('Pages/defaultTTT.py', title='OTIC TAC TOE')
+page_Home = st.Page('Pages/Home.py', title='OTIC TAC TOE')
 
 if st.session_state.login_attained == False:
   router_page = st.navigation([page_login])  
 else:
-  router_page = st.navigation([page_game])
+  router_page = st.navigation([page_Home])
 router_page.run()
