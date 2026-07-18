dd4b69e6921b6b36057c294ef6794b58caf95583
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-07-18 09:57:22 +0530

    Update defaultOTTT.py

--- Pages/defaultOTTT.py
+++ Pages/defaultOTTT.py
@@ -5,27 +5,26 @@ st.markdown('<h1 style="text-align: center;">CLASSIC TICTACTOE</h1>', unsafe_all
 
 st.divider()
 
-c1,c2,c3 = st.columns([2,1,2])
-with c2:
-  board = st.container(border=True)
-  with board:
-    st.html('''<style> div[data-testid="stLayoutWrapper"]:has(.classic-board)
-                 {border: 5px solid #2D2C35 !important;
-                  border-radius: 0px;
-                  height: 1000px;
-                  padding: 20px;}
-                </style>''')
-    
-    st.html('''<style> div[data-testid="stLayoutWrapper"]:has(.classic-board)
-                       div[data-testid="stColumn"] > div
-                       {
-                        background-color: #ffffff;
-                        }
-               </style>''')
 
-    st.html('<div class="classic-board"></div>')
+board = st.container(border=True)
+with board:
+  st.html('''<style> div[data-testid="stLayoutWrapper"]:has(.classic-board)
+  {border: 5px solid #2D2C35 !important;
+   border-radius: 0px;
+   height: 1000px;
+   padding: 20px;}
+             </style>''')
+    
+  st.html('''<style> div[data-testid="stLayoutWrapper"]:has(.classic-board)
+                     div[data-testid="stColumn"] > div
+                     {
+                      background-color: #ffffff;
+                      }
+             </style>''')
+
+  st.html('<div class="classic-board"></div>')
   
-    c4,c5,c6 = st.columns(3)
-    c7,c8,c9 = st.columns(3)
-    c10,c11,c12 = st.columns(3)
+  c4,c5,c6 = st.columns(3)
+  c7,c8,c9 = st.columns(3)
+  c10,c11,c12 = st.columns(3)
     
