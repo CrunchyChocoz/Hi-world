dbe8fa7d8110129b34ac1e24a895c47affa0dcb7
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-07-18 19:03:56 +0530

    Update defaultOTTT.py

--- Pages/defaultOTTT.py
+++ Pages/defaultOTTT.py
@@ -45,7 +45,7 @@ with c3:
   st.write('JOIN ROOM')
   with st.form('',clear_on_submit=True):
     id = st.text_input('Enter Room Code')
-    st.form_submit_button('SUBMIT', on_click=join_room)
+    st.form_submit_button('SUBMIT', on_click=join_room, args=(id))
 
 '''
 def move(id):
