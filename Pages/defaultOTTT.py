0895e633559f02ab536fee35d7bfbdd1575f5fed
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-07-18 21:17:28 +0530

    Update defaultOTTT.py

--- Pages/defaultOTTT.py
+++ Pages/defaultOTTT.py
@@ -44,10 +44,10 @@ with c3:
   with st.form('join_match',clear_on_submit=True):
     id = st.text_input('Enter Room Code')
     if st.form_submit_button('SUBMIT'):
-      if id == SUPABASE.table('ActivePlayersDB').select('*').eq('room-id',id).execute().data:
+      if id == SUPABASE.table('ActivePlayersDB').select('*').eq('room-id',id).execute().data[0]['room-id']:
         SUPABASE.table('ActivePlayersDB').update({'Player_2':st.session_state.alias}).eq('room-id',id).execute()
         st.query_params['room_id'] = id
-        st.success(f'Room ({SUPABASE.table('ActievPlayersDB').select('Player_1').eq('room-id',id).execute().data}) joined successfully')
+        st.success(f"Room ({SUPABASE.table('ActievPlayersDB').select('Player_1').eq('room-id',id).execute().data}) joined successfully")
       else:
         st.error('NO ROOM FOUND')
 
