4dbad9bab4987e07884f3beb008033f35f5f8e50
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-07-18 23:00:09 +0530

    Update defaultOTTT.py

--- Pages/defaultOTTT.py
+++ Pages/defaultOTTT.py
@@ -3,7 +3,8 @@ import random
 import supabase as sb
 
 SUPABASE = sb.create_client(st.secrets['sbURL'],st.secrets['sbPubAPI'])
-st.session_state.room_id = random.randint(1000,999999) if 'room_id' not in st.session_state else 
+if 'room-id' not in st.session_state:
+  st.session_state.room_id = random.randint(1000,999999)
 room_id = st.session_state.room_id
 st.query_params['room_id'] = room_id
 SUPABASE.table('ActivePlayersDB').insert({'room-id':room_id, 'Player_1':st.session_state.alias}).execute()
