97d1b7526a42de3b41c699db05b992bd7b466055
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-07-18 09:42:43 +0530

    Update defaultOTTT.py

--- Pages/defaultOTTT.py
+++ Pages/defaultOTTT.py
@@ -21,7 +21,7 @@ with board:
 
   st.html('''<style> div[data-testid="stLayoutWrapper"]:has(.classic-board)
                      div[data-testid="stColumn"] > div
-                     {height: 100px;
+                     {height: 100px !important;
                       background-color: #ffffff;
                       aspect-ratio: 1 !important;}
              </style>''')
