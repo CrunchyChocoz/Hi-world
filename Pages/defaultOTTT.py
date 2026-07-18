f52884aecb3724d46e32fe049cdf3b7177e89085
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-07-18 09:48:29 +0530

    Update defaultOTTT.py

--- Pages/defaultOTTT.py
+++ Pages/defaultOTTT.py
@@ -16,10 +16,6 @@ with c2:
                 </style>''')
     
     st.html('<div class="classic-board"></div>')
-      
-    c4,c5,c6 = st.columns(3)
-    c7,c8,c9 = st.columns(3)
-    c10,c11,c12 = st.columns(3)
   
     st.html('''<style> div[data-testid="stLayoutWrapper"]:has(.classic-board)
                        div[data-testid="stColumn"] > div
@@ -27,4 +23,8 @@ with c2:
                         background-color: #ffffff;
                         aspect-ratio: 1 !important;}
                </style>''')
+    
+    c4,c5,c6 = st.columns(3)
+    c7,c8,c9 = st.columns(3)
+    c10,c11,c12 = st.columns(3)
     
