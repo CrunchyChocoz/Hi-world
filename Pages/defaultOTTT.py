9394bf88fe4234af15a5aeb280f40cb352369305
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-07-26 13:13:36 +0530

    Update defaultOTTT.py

--- Pages/defaultOTTT.py
+++ Pages/defaultOTTT.py
@@ -64,7 +64,7 @@ with c2:
       st.write('READY?')
       if st.form_submit_button('ready?'):
         if st.session_state.alias == st.session_state.Player_1:
-          SUPABASE.table('ActivePlayersDB').update({'readyP1':True}).eq('room-id':st.session_state.room_id).execute()
+          SUPABASE.table('ActivePlayersDB').update({'readyP1':True}).eq('room-id',st.session_state.room_id).execute()
           st.success('READY!')
       
 with c3:
