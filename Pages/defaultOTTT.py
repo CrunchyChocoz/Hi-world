1d7fa165c88555aa992cd9ca02426c7e87439cb1
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-07-26 10:37:59 +0530

    Update defaultOTTT.py

--- Pages/defaultOTTT.py
+++ Pages/defaultOTTT.py
@@ -10,14 +10,7 @@ if 'refresh' not in st.session_state:
   st.session_state.refresh_counter = 0
   
 @st.fragment(run_every=2)
-def refresh(count):
-    while st.session_state.refresh:
-      if st.session_state.refresh and st.session_state.refresh_counter >= count:
-        break
-      else:
-        st.session_state.refresh_counter +=1
-        print('refreshed')
-        st.rerun()
+st.rerun
   
 if 'room_id' not in st.session_state:
   room_id = random.randint(1000,999999)
