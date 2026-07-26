4cad1964c98b1f44679c9472efad24f156b5b11f
CrunchyChocoz <Crunchychocoz129@gmail.com>
2026-07-26 13:05:50 +0530

    Update defaultOTTT.py

--- Pages/defaultOTTT.py
+++ Pages/defaultOTTT.py
@@ -8,19 +8,17 @@ SUPABASE = sb.create_client(st.secrets['sbURL'],st.secrets['sbPubAPI'])
 
 st_autorefresh(interval=10000)
 
-if 'refresh' not in st.session_state:
-  st.session_state.refresh = False
-  st.session_state.refresh_counter = 0
+if 'status' not in st.session_state:
+  st.session_state.status = 'waiting'
   
 if 'room_id' not in st.session_state:
   room_id = random.randint(1000,999999)
   st.query_params['room_id'] = room_id
   st.session_state.room_id = room_id
-  
-if 'status' not in st.session_state:
-  st.session_state.status = 'waiting'
 
-SUPABASE.table('ActivePlayersDB').insert({'room-id':st.session_state.room_id, 'Player_1':st.session_state.alias}).execute()
+if 'table_initiated' not in st.session_state:
+  st.session_state.table_initiated = True
+  SUPABASE.table('ActivePlayersDB').insert({'room-id':st.session_state.room_id, 'Player_1':st.session_state.alias}).execute()
 
 st.markdown('<h1 style="text-align: center;">CLASSIC TICTACTOE</h1>', unsafe_allow_html=True)
 st.divider()
@@ -35,7 +33,7 @@ with c1:
   with st.form('join_match',clear_on_submit=True):
     id_str = st.text_input('Enter Room Code')
     if st.form_submit_button('SUBMIT'):
-      if id_str == None:
+      if id_str == None or id_str == '':
         room_code = 0
       else:
         room_code = int(id_str)
@@ -44,23 +42,21 @@ with c1:
         if room_code == room[0]['room-id']:
           st.session_state.room_id = room_code
           st.query_params['room_id'] = room_code
-          st.success(f"Room ({SUPABASE.table('ActivePlayersDB').select('Player_1').eq('room-id',room_code).execute().data[0]['Player_1']}) joined successfully")
-          SUPABASE.table('ActivePlayersDB').update({'Player_2':st.session_state.alias}).eq('room-id',room_code).execute()
-          st.session_state.Player_1 = SUPABASE.table('ActivePlayersDB').select('Player_1').eq('room-id',st.session_state.room_id).execute().data[0]['Player_1']
-          st.session_state.Player_2 = SUPABASE.table('ActivePlayersDB').select('Player_2').eq('room-id',st.session_state.room_id).execute().data[0]['Player_2']
-          st.session_state.readyP1 = False
-          st.session_state.readyP2 = False
-          st.session_state.status = 'connected'
-          st.session_state.refresh = True
+          st.session_state.Player_1 = SUPABASE.table('ActivePlayersDB').select('Player_1').eq('room-id',room_code).execute().data[0]['Player_1']
+          st.session_state.Player_2 = st.session_state.alias
+          st.success(f"Room ({st.session_state.Player_1}) joined successfully")
+          SUPABASE.table('ActivePlayersDB').update({'Player_2':st.session_state.alias, 'status':'connected'}).eq('room-id',room_code).execute()
           st.rerun()
-
         else:
           st.error('NO ROOM FOUND')
       else:
         st.error('???')
 
+status = SUPABASE.table('ActivePlayersDB').select('status').eq('room-id',st.session_state.room_id).execute().data[0]['status']
+st.session_state.status = status
+
 with c2:
-  st.markdown(f"<h5 style='text-align: center;'>{SUPABASE.table('ActivePlayersDB').select('Player_1').eq('room-id',st.session_state.room_id).execute().data[0]['Player_1']}'s ROOM</h5>", unsafe_allow_html=True)
+  st.markdown(f"<h5 style='text-align: center;'>{st.session_state.Player_1}'s ROOM</h5>", unsafe_allow_html=True)
   if st.session_state.status == 'connected':
     st.write('')
     st.divider()
@@ -68,23 +64,24 @@ with c2:
       st.write('READY?')
       if st.form_submit_button('ready?'):
         if st.session_state.alias == st.session_state.Player_1:
-          st.session_state.readyP1 = True
+          SUPABASE.table('ActivePlayersDB').update({'readyP1':True).eq('room-id':st.session_state.room_id).execute()
           st.success('READY!')
       
 with c3:
-  Player_2 = SUPABASE.table('ActivePlayersDB').select('Player_2').eq('room-id',st.session_state.room_id).execute().data[0]['Player_2']
   if st.session_state.status == 'connected':
-    st.markdown(f"<h5 style='text-align: center;'>{Player_2} connected</h5>", unsafe_allow_html=True)
+    st.markdown(f"<h5 style='text-align: center;'>{st.session_state.Player_2} connected</h5>", unsafe_allow_html=True)
     st.write('')
     st.divider()
     with st.form('P2'):
       st.write('READY?')
       if st.form_submit_button('ready?'):
         if st.session_state.alias == st.session_state.Player_2:
-          st.session_state.readyP2 = True
+          SUPABASE.table('ActivePlayersDB').update({'readyP2':True}).eq('room-id':st.session_state.room_id).execute()
           st.success('READY!')
-          
-  
+
+if 'game_start' not in st.session_state:
+  ready_status = SUPABASE.table('ActivePlayersDB').select('readyP1,readyP2').eq('room-id':st.session_state.room_id).execute.data[0]
+  st.session_state.game_start = True if (ready_status['readyP1'] and ready_status['readyP2']) else False
 
 if 'turn' not in st.session_state:
   if random.randint(0,1) == 0:
@@ -138,7 +135,7 @@ def create_board():
           if state:
             st.write(board_data[0][f'Tile{index}'])
           else:
-            st.button('',key=f'tile{index}' ,on_click=move(), args=(index))
+            st.button('',key=f'tile{index}' ,on_click=move, args=(index))
 
 def get_game_id():
     query = SUPABASE.table('defaultOTTTDB').insert({})
